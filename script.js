bb1f1f5c368ddec3071db2401a14eb7a01b01b51
luvsense <yvsh7.btw@gmail.com>
2024-04-14 21:57:27 +0530

    First working version

---
+++ script.js
@@ -0,0 +1,71 @@
+const output = document.querySelector(".calculator__output");
+const operatorButtons = document.querySelectorAll(".calculator__key--operator");
+const numberButtons = document.querySelectorAll(".number-key");
+const result = document.querySelector(".calculator__key--enter");
+const clear = document.querySelector(".clear-btn");
+
+let currentNumber = "";
+let previousNumber = "";
+
+let operator = "";
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    let number = this.value;
+    currentNumber += number;
+    output.textContent = currentNumber;
+  });
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    operator = this.value;
+    previousNumber = currentNumber;
+    currentNumber = "";
+  });
+});
+
+result.addEventListener("click", function () {
+  let result;
+  if (operator === "+") {
+    result = add(Number(previousNumber), Number(currentNumber));
+  } else if (operator === "-") {
+    result = subtract(Number(previousNumber), Number(currentNumber));
+  } else if (operator === "*") {
+    result = multiply(Number(previousNumber), Number(currentNumber));
+  } else if (operator == "/") {
+    result = divide(Number(previousNumber), Number(currentNumber));
+  }
+  output.textContent = result;
+});
+
+function add(number1, number2) {
+  const sum = number1 + number2;
+  return sum;
+}
+function subtract(number1, number2) {
+  const result = number1 - number2;
+  return result;
+}
+
+function multiply(number1, number2) {
+  const result = number1 * number2;
+  return result;
+}
+
+function divide(number1, number2) {
+  const result = number1 / number2;
+  if (number1 > 0 && number2 === 0) {
+    return "Undefined";
+  } else if (number1 === 0 && number2 === 0) {
+    return "Infinity";
+  } else {
+    return result;
+  }
+}
+
+clear.addEventListener("click", function () {
+  output.textContent = "0";
+  previousNumber = "";
+  currentNumber = "";
+});
